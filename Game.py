7e8514275358adb7fe6e3044b1babdc3712e00b9
DaigoDark19 <Diegomaluenga19@gmail.com>
2024-04-19 18:16:30 -0400

    python PEP 8

--- Game.py
+++ Game.py
@@ -1,8 +1,7 @@
 import pygame
-from sprite import *
-from personajes import *
+from personajes import knight, enemy, Boss
+import time
 import random
-
 
 
 pygame.init()
@@ -11,48 +10,51 @@ screen = pygame.display.set_mode((1280, 670))
 pygame.display.set_caption("Fake Souls")
 running = True
 
-# # Inicializa el mezclador de música
-# pygame.mixer.init()
-# # Carga la música
-# pygame.mixer.music.load('music\Soul-of-Cinder-True-8-Bit.mp3')
-# # Reproduce la música
-# pygame.mixer.music.play(-1)  # El -1 hace que la música se repita indefinidamente
+# Inicializa el mezclador de música
+pygame.mixer.init()
+# Carga la música
+pygame.mixer.music.load(
+    'music\\Soul-of-Cinder-True-8-Bit.mp3'
+    )
+# Reproduce la música
+# El -1 hace que la música se repita indefinidamente
+pygame.mixer.music.play(-1)
 
 
-clock=pygame.time.Clock()
+clock = pygame.time.Clock()
 
 
-background=[
-    pygame.image.load("background\PNG\Battleground1\Bright\Battleground1.png").convert(),
-    pygame.image.load("background\PNG\Battleground4\Bright\Battleground4.png").convert(),
-    pygame.image.load("background\PNG\Battleground2\Bright\Battleground2.png").convert()
+background = [
+    pygame.image.load(
+        "background\\PNG\\Battleground1\\Bright\\Battleground1.png"
+        ).convert(),
+
+    pygame.image.load(
+        "background\\PNG\\Battleground4\\Bright\\Battleground4.png"
+        ).convert(),
+
+    pygame.image.load(
+        "background\\PNG\\Battleground2\\Bright\\Battleground2.png"
+        ).convert()
 ]
 
 
-
-
-
-        
-
-
-
-
-player=knight()
-boss=Boss()
-esqueletoS=enemy(0)
-esqueletoL=enemy(1)
+player = knight()
+boss = Boss()
+esqueletoS = enemy(0)
+esqueletoL = enemy(1)
 
 boss.Move = boss.inicio()
-indBack=0
-Background=background[indBack]
+indBack = 0
+ataqueR = 3
+
+Background = background[indBack]
 
 while running:
     events = pygame.event.get()
     for event in events:
         if event.type == pygame.QUIT:
             running = False
-
-   
 
     # Actualización del fondo
     if player.newposx > 1230 and indBack <= 1:
@@ -64,137 +66,147 @@ while running:
         Background = background[indBack]
         player.newposx = 1200
 
-     # Dibujar el fondo
+    # Dibujar el fondo
     screen.blit(Background, [0, 0])
 
+    # seleccionamos al enemigo a operar
+    if indBack == 0:
+        enemigo = esqueletoS
+    if indBack == 1:
+        enemigo = esqueletoL
+    if indBack == 2:
+        enemigo = boss
 
-    #seleccionamos al enemigo a operar
-    if indBack==0:
-        enemigo=esqueletoS
-    if indBack==1:
-        enemigo=esqueletoL
-    if indBack==2:
-        enemigo=boss
-    
-    
+    # chekeadores de enemigo
+    VIDACHECKP = player.vida != 0
+    PLAYERDENE = abs(player.newposx - enemigo.newposx) <= 100
+    PLAYERY_400 = player.newposy == 400
+    PLAYERMEIENEMIGO_300 = player.newposx <= enemigo.newposx + 300
+    PLAYERMEIENEMIGO_110 = player.newposx <= enemigo.newposx + 110
+    PLAYERMAIENEMIGO_400 = player.newposx >= enemigo.newposx - 300
+    PLAYERMAIENEMIGO_120 = player.newposx >= enemigo.newposx - 110
+    PLAYERMAIENEMIGO = player.newposx >= enemigo.newposx
+    PLAYERMEIENEMIGO = player.newposx <= enemigo.newposx
 
-    #enemigo detecta posicion del personaje y ataca
-    if indBack==0 or indBack==1:
-        if enemigo.vida==1:
+    # enemigo detecta posicion del personaje y ataca
+    if indBack == 0 or indBack == 1:
+        if enemigo.vida == 1:
             # Actualización y dibujo del enemigo
             if enemigo.walking:
                 enemigo.move = enemigo.movE()
                 enemigo.pintarE(screen, background)
 
-            if player.newposx>=enemigo.newposx and player.newposx<=enemigo.newposx+300 and player.vida!=0:
-                enemigo.walking=False
-                enemigo.direction=False
+            if PLAYERMAIENEMIGO and PLAYERMEIENEMIGO_300 and VIDACHECKP:
+                enemigo.walking = False
+                enemigo.direction = False
                 enemigo.atta = enemigo.attack()
-                hit=enemigo.atta.animationF
+                hit = enemigo.atta.animationF
                 enemigo.pintarE(screen, background)
-                if player.newposx>=enemigo.newposx and player.newposx<=enemigo.newposx+110 and player.newposy==400 and hit:
-                    player.hurting=True
-                    player.vida-=1
-                    player.newposx+=100
-                    
-            elif player.newposx<=enemigo.newposx and player.newposx>=enemigo.newposx-300 and player.vida!=0:
-                enemigo.walking=False
-                enemigo.direction=True
+                if PLAYERMEIENEMIGO_110 and PLAYERY_400 and hit:
+                    player.hurting = True
+                    player.vida -= 1
+                    player.newposx += 100
+
+            elif PLAYERMEIENEMIGO and PLAYERMAIENEMIGO_400 and VIDACHECKP:
+                enemigo.walking = False
+                enemigo.direction = True
                 enemigo.atta = enemigo.attack()
-                hit=enemigo.atta.animationF
+                hit = enemigo.atta.animationF
                 enemigo.pintarE(screen, background)
-                if player.newposx<=enemigo.newposx and player.newposx>=enemigo.newposx-110 and player.newposy==400 and hit:
-                    player.hurting=True
-                    player.vida-=1
-                    player.newposx-=100
-                    
-                    
+                if PLAYERMAIENEMIGO_120 and PLAYERY_400 and hit:
+                    player.hurting = True
+                    player.vida -= 1
+                    player.newposx -= 100
+
             else:
-                enemigo.walking=True
-        if enemigo.vida==0 and enemigo.Dead.animationF==False:
-            enemigo.Dead=enemigo.dead()
-            enemigo.pintarE(screen,background)
+                enemigo.walking = True
+
+        if enemigo.vida == 0 and enemigo.Dead.animationF is False:
+            enemigo.Dead = enemigo.dead()
+            enemigo.pintarE(screen, background)
 
     else:
-            boss.corazon(screen)
-            ataqueR= random.randint(1, 3)
+        # CODIGO DEL BOSS:
+        boss.corazon(screen)
 
-            if boss.vida==0:
-                boss.Move=boss.Dead()
+        # chekeadores del boss:
+        PLAYERMEIBOSS = player.newposx <= boss.newposx
+        PLAYERMAIBOSS = player.newposx >= boss.newposx
+        PLAYERMAIBOSS_450 = player.newposx >= boss.newposx-450
+        PLAYERMEIBOSS_450 = player.newposx <= boss.newposx+450
+        PLAYERDBOSS = abs(player.newposx - enemigo.newposx) <= 150
 
+        if boss.vida == 0:
+            boss.Move = boss.Dead()
+
+            if boss.Move.animationF:
+                time.sleep(2)
+                running = False
+        else:
+
+            if boss.ini:
+                boss.Move = boss.inicio()
                 if boss.Move.animationF:
-                    time.sleep(2)
-                    running=False
+                    boss.ini = False
             else:
-                #CODIGO DEL BOSS:
-                if boss.ini:
-                    boss.Move = boss.inicio()
-                    if boss.Move.animationF:
-                        boss.ini=False
+
+                if PLAYERMEIBOSS and PLAYERMAIBOSS_450 and VIDACHECKP:
+                    boss.direction = True
+                    boss.Move = boss.Run()
+                    if -abs(player.newposx - enemigo.newposx) <= -150:
+                        boss.newposx -= 6
+                elif PLAYERMAIBOSS and PLAYERMEIBOSS_450 and player.vida != 0:
+                    boss.direction = False
+                    boss.Move = boss.Run()
+                    if abs(player.newposx - enemigo.newposx) >= 150:
+                        boss.newposx += 6
                 else:
-                    
+                    boss.Move = boss.move()
 
-                    if player.newposx<=boss.newposx and player.newposx>=boss.newposx-450 and player.vida!=0:
-                        boss.direction=True
-                        boss.Move=boss.Run()
-                        if -abs(player.newposx - enemigo.newposx) <= -150 :
-                            boss.newposx-=6
-                    elif player.newposx>=enemigo.newposx and player.newposx<=enemigo.newposx+450 and player.vida!=0:
-                            boss.direction=False
-                            boss.Move=boss.Run()
-                            if abs(player.newposx - enemigo.newposx) >= 150 :
-                                boss.newposx+=6
-                    else:
-                        boss.Move=boss.move()
+                if PLAYERDBOSS and PLAYERY_400 and VIDACHECKP:
+                    boss.Move = boss.atacar(ataqueR)
+                    if boss.Move.animationF:
+                        ataqueR = random.randint(1, 3)
+                        player.vida -= 1
+                        boss.Move.animationF = False
 
-                    if abs(player.newposx - enemigo.newposx) <= 150 and player.newposy==400 and player.vida!=0:
-                         boss.Move=boss.atacar(ataqueR)
-                         if boss.Move.animationF:
-                            player.vida-=1
-                            boss.Move.animationF=False
-                        
-                
-            boss.pintar(screen,background)
+            boss.pintar(screen)
             # boss.saltar(screen)
 
-
-
     player.corazon(screen)
-    
-    #Jugador muerte GAME OVER
-    if player.vida==0:
-        player.Dead=player.dead()
-        player.pintar(screen,background)
-        f=player.Dead.animationF
+
+    # Jugador muerte GAME OVER
+    if player.vida == 0:
+        player.Dead = player.dead()
+        player.pintar(screen, background)
+        f = player.Dead.animationF
         if f:
             time.sleep(2)
-            running=False
+            running = False
     else:
         # Actualización y dibujo del personaje
-        player.mov= player.mover(events, indBack)
+        player.mov = player.mover(events, indBack)
 
         if player.isjump:
             player.saltar(screen)
         else:
             player.pintar(screen, Background)
-    
 
-    #atacar al enemigo:
-    if abs(player.newposx - enemigo.newposx) <= 100 and player.newposy == 400 and player.ataque:
-        if enemigo!=boss:
+    # atacar al enemigo:
+    if PLAYERDENE and PLAYERY_400 and player.ataque is True:
+        if enemigo != boss and enemigo.vida > 0:
             enemigo.vida -= 1
-            player.vida+=1
-        else:
+            player.vida += 1
+
+        if enemigo == boss:
             enemigo.vida -= 1
-            if boss.direction==False:
-                boss.newposx-=250
+            if boss.direction is False:
+                boss.newposx -= 250
             else:
-                boss.newposx+=250
-            
-    player.ataque=False    
+                boss.newposx += 250
 
-    
-            
+    player.ataque = False
+
     # Actualizar la pantalla
     pygame.display.flip()
 
--- sprite.py
+++ sprite.py
@@ -2,88 +2,97 @@ import pygame
 import time
 
 
-
 class SpriteAnimation(pygame.sprite.Sprite):
-    def __init__(self, sp, i2, anc, alt, fps):
+    def __init__(self, sp: str, i2: int, anc: int, alt: int, fps: float):
         super().__init__()
-        #toma la ruta de la imagen hasta el nombre de la imagen sin contar su extencion (obligatoriamente tiene que ser png)
+        # toma la ruta de la imagen hasta el nombre de la imagen
+        # sin contar su extencion (obligatoriamente tiene que ser png)
         self.sp = sp
-        #indica la cantida de sprite que tiene la animacion
+        # indica la cantida de sprite que tiene la animacion
         self.i2 = i2
 
-        #el tamano de la imagen
-        self.anc=anc
-        self.alt=alt
+        # el tamano de la imagen
+        self.anc = anc
+        self.alt = alt
 
-    
         self.indSprite = 0
 
-        self.cont=0
-       
-        #es la pausa por cada frame que va a tener para el cambio de cada imgaen
-        self.fps=fps
+        self.cont = 0
 
-        self.animationF = False  # variable para rastrear si la animación ha terminado
+        # es la pausa por cada frame
+        # que va a tener para el cambio de cada imgaen
+        self.fps = fps
+
+        # variable para rastrear si la animación ha terminado
+        self.animationF = False
 
         # Cargamos los sprites al inicializar
         self.sprites = self.cargadesrpite()
 
-
-
     def cargadesrpite(self):
         # Cargamos las imágenes del sprite en una lista
-        spriteArray = [pygame.image.load(f'{self.sp}{i}.png').convert_alpha() for i in range(self.i2)]
-        #lo pasamos a otra lista para cambiar su escalado
-        sprite_escalado = [pygame.transform.scale(spriteArray[i], (self.anc, self.alt)) for i in range(self.i2)]
+        spriteArray = [
+            pygame.image.load(f'{self.sp}{i}.png').convert_alpha()
+            for i in range(self.i2)
+            ]
+        # lo pasamos a otra lista para cambiar su escalado
+        sprite_escalado = [
+            pygame.transform.scale(spriteArray[i], (self.anc, self.alt))
+            for i in range(self.i2)
+            ]
         return sprite_escalado
 
-    def animation(self, screen,posx,posy,flipx):
+    def animation(self, screen, posx, posy, flipx):
 
-        SpriteFlip = [pygame.transform.flip(sprite, flipx, False) for sprite in self.sprites]
-        
+        SpriteFlip = [
+            pygame.transform.flip(sprite, flipx, False)
+            for sprite in self.sprites
+            ]
+
         # Dibujamos el sprite actual en la pantalla
-        screen.blit(SpriteFlip[self.indSprite], (posx,posy))
+        screen.blit(SpriteFlip[self.indSprite], (posx, posy))
 
-        
-        self.cont+=1
+        self.cont += 1
 
-        if self.cont==self.fps:
+        if self.cont == self.fps:
             # Actualizamos el índice del sprite (cambiamos al siguiente sprite)
             self.indSprite = (self.indSprite + 1) % len(self.sprites)
 
-            self.cont=0
-            
-            #detecta si ya se llego al ultimo sprite
+            self.cont = 0
+
+            # detecta si ya se llego al ultimo sprite
             if self.indSprite == len(self.sprites) - 1:
                 self.animationF = True
             else:
                 self.animationF = False
 
-    
+    def animation_2(
+            self, screen: str, posx: int, posy: int, flipx: bool,
+            background: str
+            ):
 
-    def animation_2(self, screen,posx,posy,flipx,background):
-       SpriteFlip = [pygame.transform.flip(sprite, flipx, False) for sprite in self.sprites]
+        SpriteFlip = [
+            pygame.transform.flip(sprite, flipx, False)
+            for sprite in self.sprites
+            ]
 
-       for i in range(self.i2):
-        # Crea un rectángulo que cubre el sprite
-        sprite_rect = pygame.Rect(posx, posy, SpriteFlip[i].get_width(), SpriteFlip[i].get_height())
-        
-        # Dibuja una porción del fondo sobre el sprite
-        screen.blit(background, (posx, posy), sprite_rect)
-        
-        # Dibuja el sprite
-        screen.blit(SpriteFlip[i], (posx, posy))
+        for i in range(self.i2):
+            # Crea un rectángulo que cubre el sprite
+            sprite_rect = pygame.Rect(
+                posx, posy, SpriteFlip[i].get_width(),
+                SpriteFlip[i].get_height()
+                )
 
+            # Dibuja una porción del fondo sobre el sprite
+            screen.blit(background, (posx, posy), sprite_rect)
 
-        if i==self.i2:
-            self.animationF = True
-        else:
-            self.animationF = False
+            # Dibuja el sprite
+            screen.blit(SpriteFlip[i], (posx, posy))
 
-        pygame.display.flip()
-        time.sleep(self.fps)
+            if i == self.i2:
+                self.animationF = True
+            else:
+                self.animationF = False
 
-
-
-
-   
+            pygame.display.flip()
+            time.sleep(self.fps)
